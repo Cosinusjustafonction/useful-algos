5c943d46c7be0584685edc939d8bfd50b648809d
ilyass <64425437+Cosinusjustafonction@users.noreply.github.com>
2022-05-17 14:03:14 +0100

    Create generating subsets.cpp

---
+++ generating subsets.cpp
@@ -0,0 +1,13 @@
+//generating subsets ; 
+void search(int k){
+    if (k==n){
+
+        //process subset
+    }
+    else{
+        search(k+1) ; 
+        subset.push_back(k) ; 
+        search(k+1) ; 
+        subset.pop_back() ; 
+    }
+}
